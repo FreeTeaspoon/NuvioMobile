2b9105ace98c345cbad607cf789cb39a6d2ff893
Shen <664203@trinity.vic.edu.au>
2026-04-28 01:52:51 +1000

    Build fork releases from official releases

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/updater/AppUpdater.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/updater/AppUpdater.kt
@@ -53,7 +53,7 @@ import nuvio.composeapp.generated.resources.*
 import org.jetbrains.compose.resources.getString
 import org.jetbrains.compose.resources.stringResource
 
-private const val gitHubOwner = "NuvioMedia"
+private const val gitHubOwner = "FreeTeaspoon"
 private const val gitHubRepo = "NuvioMobile"
 private const val gitHubApiBase = "https://api.github.com"
 private const val releaseChannelBranch = "cmp-rewrite"
